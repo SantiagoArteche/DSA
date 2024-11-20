42d4a3ee6aae536ce717bd4d0c6482f3ba6bdc81
santiago arteche <santiarteche@hotmail.com>
2024-11-20 01:32:47 -0300

    second commit

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,7 +1,14 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1, 5, 10, 15, 20, 33, 44, 55, 66, 77, 88, 99, 102, 106, 109, 1100, 1500, 1600};
-        System.out.println(binarySearch(arr, 55));
+//        System.out.println(binarySearch(arr, 55));
+        System.out.println(orderReversedBinarySearch(arr, 55));
     }
 
 
@@ -31,4 +38,30 @@ public class BinarySearch {
 
         return -1;
     }
+
+    public static int orderReversedBinarySearch(int[] array, int target){
+        int[] reversedArray = new int[array.length];
+
+        for(int i = array.length -1, j = 0; i >= 0; i--, j++){
+            reversedArray[j] = array[i];
+        }
+
+        int start = 0;
+        int end = reversedArray.length - 1;
+
+        while (start <= end){
+            int middle = start + (end - start) / 2;
+
+            if(reversedArray[middle] > target){
+                start = middle + 1;
+            }else if(reversedArray[middle] < target){
+                end = middle - 1;
+            }else{
+                return middle;
+            }
+        }
+        return -1;
+
+    }
+
 }
