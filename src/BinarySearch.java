fad12ab50b48040b535ca7a95355f07e5c81d89d
santiago arteche <santiarteche@hotmail.com>
2024-11-20 19:56:47 -0300

    solving bs problems

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1, 5, 10, 15, 20, 33, 44, 55, 66, 77, 88, 99, 102, 106, 109, 1100, 1500, 1600};
-//        System.out.println(binarySearch(arr, 55));
-        System.out.println(orderReversedBinarySearch(arr, 55));
+        System.out.println(binarySearch(arr, 55));
+//        System.out.println(orderReversedBinarySearch(arr, 55));
     }
 
 
@@ -19,24 +19,18 @@ public class BinarySearch {
         int end = arr.length - 1;
 
         while(start <= end){
-            // s + (e - s) / 2 === 2s + e - s / 2 (2s and the second s are cancelled) so (s + e) / 2
-            int middle = start + (end - start) / 2; // (start + end) / 2 might exceed the range of integers
-            // middle = 8, start = 0, end = 17
-            // start = 0, end = 7 middle = 3
-            // start = 4, end = 7, middle 5
-            // start = 6, end = 7 , middle = 6
-            // done middle = 7 end = 7 start =7 (WHEN TARGET == 55)
+            int middle = start + (end - start) / 2;
 
-            if(arr[middle] < target){
-                start = middle + 1;
-            }else if(arr[middle] > target){
+            if(target > arr[middle]){
+                start =  middle + 1;
+            }else if(target < arr[middle]){
                 end = middle - 1;
-            }else{
+            }else {
                 return middle;
             }
         }
 
-        return -1;
+        return  -1;
     }
 
     public static int orderReversedBinarySearch(int[] array, int target){
@@ -61,7 +55,5 @@ public class BinarySearch {
             }
         }
         return -1;
-
     }
-
 }
---
+++ src/BinarySearchProblems.java
@@ -0,0 +1,107 @@
+public class BinarySearchProblems {
+    public static void main(String[] args) {
+        int[] testArray = {1, 2, 5, 10, 16, 19, 22 , 55, 66, 100, 1115, 2000, 4555, 9000, 15015};
+
+        System.out.println(ceilingEqual(testArray, 115));
+        System.out.println(floorEqual(testArray, 15));
+        System.out.println(infiniteArray(testArray, 15015));
+        System.out.println(nextGreatestLetter(new char[]{'c','f','j'}, 'a'));
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        //You are given an array of characters letters that is sorted in non-decreasing order, and a character target.
+        // There are at least two different characters in letters.
+        //Return the smallest character in letters that is lexicographically greater than target.
+        // If such a character does not exist, return the first character in letters.
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target < letters[mid]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        return letters[start % letters.length];
+    }
+
+    static int infiniteArray(int[] arr, int target){
+        int start = 0;
+        int end = 1;
+
+        int ans = binarySearch(arr, target, start, end);
+
+        while (ans == -1){
+            int newStart = end + 1;
+            end = end + (end - start + 1) * 2;
+            ans = binarySearch(arr, target, newStart, end);
+        }
+
+        return ans;
+    }
+
+    static int ceilingEqual(int[] arr, int target){
+        if(arr.length == 0) return -1;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] > target){
+                end = mid - 1;
+            }else if(arr[mid] < target){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return start;
+    }
+
+    static int floorEqual(int[] arr, int target){
+        if(arr.length == 0) return -1;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] > target){
+                end = mid - 1;
+            }else if(arr[mid] < target){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+
+        return end;
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end){
+        if (arr.length == 0) return -1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > arr[mid]){
+                start = mid + 1;
+            }else if(target < arr[mid]){
+                end = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
+
+
