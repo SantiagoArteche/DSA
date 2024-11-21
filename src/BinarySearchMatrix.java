5d54f5a511e928024a0c1dfea98a28dcaad07783
santiago arteche <santiarteche@hotmail.com>
2024-11-21 14:52:22 -0300

    binarySearch in matrix

---
+++ src/BinarySearchMatrix.java
@@ -0,0 +1,104 @@
+import java.util.Arrays;
+
+public class BinarySearchMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+
+        int[][] matrixSorted = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+
+        System.out.println(Arrays.toString(findInMatrix(matrix, 38)));;
+        System.out.println(Arrays.toString(findInSortedMatrix(matrixSorted, 8)));;
+    }
+
+    static int[] findInSortedMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        if (rows == 1) {
+            return binarySearch(matrix, 0, 0, cols - 1, target);
+        }
+
+        int rStart = 0;
+        int rEnd = rows - 1;
+        int cMid = cols / 2;
+
+        while (rStart < rEnd - 1) {
+            int mid = rStart + (rEnd - rStart) / 2;
+
+            if (matrix[mid][cMid] == target) {
+                return new int[]{mid, cMid};
+            } else if (matrix[mid][cMid] < target) {
+                rStart = mid;
+            } else {
+                rEnd = mid;
+            }
+        }
+
+        if (target <= matrix[rStart][cMid - 1]) {
+            return binarySearch(matrix, rStart, 0, cMid - 1, target);
+        }
+
+        if (target >= matrix[rStart][cMid + 1] && target <= matrix[rStart][cols - 1]) {
+            return binarySearch(matrix, rStart, cMid + 1, cols - 1, target);
+        }
+
+        if (target <= matrix[rEnd][cMid - 1]) {
+            return binarySearch(matrix, rEnd, 0, cMid - 1, target);
+        }
+
+        if (target >= matrix[rEnd][cMid + 1] && target <= matrix[rEnd][cols - 1]) {
+            return binarySearch(matrix, rEnd, cMid + 1, cols - 1, target);
+        }
+
+
+        return new int[]{-1, -1};
+    }
+
+
+    static int[] binarySearch(int[][] matrix, int row, int colStart, int colEnd, int target){
+        while (colStart <= colEnd){
+            int mid = colStart + (colEnd - colStart) / 2;
+
+            if (matrix[row][mid] == target) {
+                return new int[]{row, mid};
+            } else if (matrix[row][mid] > target) {
+                colEnd = mid - 1;
+            } else {
+                colStart = mid + 1;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    static int[] findInMatrix(int[][] matrix, int target){
+            int row = 0;
+            int col = matrix.length - 1;
+
+            while (row < matrix.length && col >= 0){
+                if(matrix[row][col] == target){
+                    return new int[]{row, col};
+                }
+
+                if(matrix[row][col] > target){
+                    col--;
+                }else{
+                    row++;
+                }
+            }
+
+        return new int[]{-1, -1};
+    }
+
+}
+
+
