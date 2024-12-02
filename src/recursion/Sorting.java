d63ce1a99232e7ff9e1602381d14b8ccbb2b1abf
santiago arteche <santiarteche@hotmail.com>
2024-12-02 05:48:37 -0300

    merge sort

--- src/recursion/Sorting.java
+++ src/recursion/Sorting.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         int[] arr = {4,5,9,1,2,3,11,23, -3};
-        selectionSort(arr, arr.length, 0, 0);
+        inPlaceMergeSort(arr, 0 , arr.length);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -39,4 +39,98 @@ public class Sorting {
             arr[row - 1] = temp;
             selectionSort(arr, row - 1, 0,  0);
         }
-    }}
+    }
+
+    public static int[] mergeSort(int[] arr){
+        if(arr.length == 1) return arr;
+
+
+        int mid = arr.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] left, int[] right){
+        int[] mergedArr = new int[left.length + right.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                mergedArr[k] = left[i];
+                i++;
+            }else{
+                mergedArr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < left.length){
+            mergedArr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while(j < right.length){
+            mergedArr[k] = right[j];
+            j++;
+            k++;
+        }
+
+        return mergedArr;
+    }
+
+    private static void inPlaceMergeSort(int[] arr, int start, int end){
+        if(end - start == 1) return;
+
+        int mid = start + (end - start) / 2;
+
+        inPlaceMergeSort(arr, start, mid);
+        inPlaceMergeSort(arr, mid, end);
+
+        mergeInPlace(arr, start, mid, end);
+    }
+
+    public static void mergeInPlace(int[] arr, int start, int mid, int end){
+        int[] mergedArr = new int[end - start];
+
+        int i = start;
+        int j = mid;
+        int k = 0;
+
+        while(i < mid && j < end){
+            if(arr[i] < arr[j]){
+                mergedArr[k] = arr[i];
+                i++;
+            }else{
+                mergedArr[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < mid){
+            mergedArr[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while(j < end){
+            mergedArr[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for(int l = 0; l < mergedArr.length; l++){
+            arr[start + l] = mergedArr[l];
+        }
+    }
+}
+
+
