bb74aca8ab0809ee468274971c33f545e44ac325
santiago arteche <santiarteche@hotmail.com>
2024-12-02 22:38:37 -0300

    permutation

--- src/recursion/Permutation.java
+++ src/recursion/Permutation.java
@@ -2,6 +2,37 @@ package recursion;
 
 public class Permutation {
     public static void main(String[] args){
+        permutations("", "abc");
+        System.out.println(permutationCount("", "abc"));
+    }
 
+    static void permutations(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        for(int i = 0; i <= p.length(); i++){
+            String fs = p.substring(0, i);
+            String sc = p.substring(i);
+            permutations(fs + ch + sc,  up.substring(1));
+        }
+    }
+
+    static int permutationCount(String p, String up){
+        if(up.isEmpty()) return 1;
+
+        char ch = up.charAt(0);
+        int count = 0;
+
+        for(int i = 0; i <= p.length(); i++){
+            String fs = p.substring(0, i);
+            String sc = p.substring(i);
+            count += permutationCount(fs + ch + sc, up.substring(1));
+        }
+
+        return count;
     }
 }
