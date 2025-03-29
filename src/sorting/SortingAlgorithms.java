3ada1e55e9d8e41fdf25041deefc2c4608211a33
santiago arteche <santiarteche@hotmail.com>
2025-03-29 13:37:33 -0300

    radix sort

--- src/sorting/SortingAlgorithms.java
+++ src/sorting/SortingAlgorithms.java
@@ -1,5 +1,6 @@
 package sorting;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class SortingAlgorithms {
@@ -23,6 +24,10 @@ public class SortingAlgorithms {
         int[] arr5 = {3, 4, 1, 3, 2, 5, 2};
         countSort(arr5);
         showArray(arr5);
+
+        int[] arr6 = {29, 83, 471, 36, 91, 8, 2};
+        radixSort(arr6);
+        showArray(arr6);
     }
 
     static void cyclicSort(int[] arr){
@@ -118,6 +123,47 @@ public class SortingAlgorithms {
             }
         }
 
+    }
+
+    static void radixSort(int[] arr){
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        for(int exp = 1; max / exp > 0; exp *= 10){
+            countSortForRadix(arr, exp);
+        }
+    }
+
+    static void countSortForRadix(int[] arr, int exp){
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for(int i = 0; i < count.length; i++){
+            count[i] = 0;
+        }
+
+        for(int j = 0; j < n ; j++){
+            count[(arr[j] / exp) % 10]++;
+        }
+
+        System.out.println("Count array for " + exp + " : " + Arrays.toString(count));
+        for(int k = 1; k < 10; k++){
+            count[k] = count[k] + count[k - 1];
+        }
+
+        System.out.println("Updated count array for " + exp + " : " + Arrays.toString(count));
+
+        for(int l = n - 1; l >= 0; l--){
+            output[count[(arr[l] / exp) % 10] - 1] = arr[l];
+            count[(arr[l] / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, n);
     }
 
     static void showArray(int[] arr) {
